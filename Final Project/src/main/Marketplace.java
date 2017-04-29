e648fa258dc6b29b0053dfe424b5ae85269e5c03
Nate Pellant <npellant@umail.iu.edu>
2017-04-28 21:09:01 -0400

    Marketplace formatting

--- Final Project/src/main/Marketplace.java
+++ Final Project/src/main/Marketplace.java
@@ -11,8 +11,9 @@ import java.util.Scanner;
  * @author MattOndash
  * @author NatePellant
  *
- *<p>
- *Marketplace is the logical representation of the online store - it handles most logic events for the project
+ *         <p>
+ *         Marketplace is the logical representation of the online store - it
+ *         handles most logic events for the project
  */
 public class Marketplace {
 
@@ -24,7 +25,8 @@ public class Marketplace {
 	private static double SELLER_REVENUE_PERCENTAGE = .9;
 
 	/**
-	 * Marketplace Constructor: initializes new Directory and Inventory, sets current Account to be null
+	 * Marketplace Constructor: initializes new Directory and Inventory, sets
+	 * current Account to be null
 	 */
 	public Marketplace() {
 
@@ -34,24 +36,28 @@ public class Marketplace {
 	}
 
 	/**
-	 * increments the ID counter 
+	 * increments the ID counter
+	 * 
 	 * @return the value of the counter after being incremented
 	 */
 	private static int assignID() {
 		ID++;
 		return ID;
 	}
-	
+
 	/**
 	 * Sets the percentage of profits a Seller receives for sales
-	 * @param percentage - the percentage to set (in decimal form)
+	 * 
+	 * @param percentage
+	 *            - the percentage to set (in decimal form)
 	 */
 	public static void setSellerRevenuePercentage(double percentage) {
 		SELLER_REVENUE_PERCENTAGE = percentage;
 	}
-	
+
 	/**
-	 * @return the percentage of profits a Seller receives for sales (in decimal form)
+	 * @return the percentage of profits a Seller receives for sales (in decimal
+	 *         form)
 	 */
 	public static double getSellerRevenuePercentage() {
 		return SELLER_REVENUE_PERCENTAGE;
@@ -59,9 +65,13 @@ public class Marketplace {
 
 	/**
 	 * Adds Account to the Marketplace's Directory with new generated ID
-	 * @param name - the name of the Account to add
-	 * @param password - the password of the Account to add
-	 * @param type - the type of the Account to add
+	 * 
+	 * @param name
+	 *            - the name of the Account to add
+	 * @param password
+	 *            - the password of the Account to add
+	 * @param type
+	 *            - the type of the Account to add
 	 */
 	public void addAccount(String name, char[] password, String type) {
 
@@ -71,12 +81,19 @@ public class Marketplace {
 
 	/**
 	 * Adds a Product to the Marketplace's Inventory with new generated ID
-	 * @param sellerID - the ID of the Seller of the Product
-	 * @param name - the name of the Product
-	 * @param description - short description of the Product
-	 * @param category - the category of the Product
-	 * @param price - the price of the Product
-	 * @param quantity - the amount of the Product
+	 * 
+	 * @param sellerID
+	 *            - the ID of the Seller of the Product
+	 * @param name
+	 *            - the name of the Product
+	 * @param description
+	 *            - short description of the Product
+	 * @param category
+	 *            - the category of the Product
+	 * @param price
+	 *            - the price of the Product
+	 * @param quantity
+	 *            - the amount of the Product
 	 */
 	public void addProduct(int sellerID, String name, String description, String category, double price, int quantity) {
 		inventory.add(new Product(assignID(), sellerID, name, description, category, price, quantity));
@@ -84,8 +101,11 @@ public class Marketplace {
 
 	/**
 	 * Attempts to log in to the Marketplace with given credentials
-	 * @param name - the name of the Account to attempt access 
-	 * @param password - the password of the Account to attempt access
+	 * 
+	 * @param name
+	 *            - the name of the Account to attempt access
+	 * @param password
+	 *            - the password of the Account to attempt access
 	 * @return whether or not login attempt is successful
 	 */
 	public boolean tryLogin(String name, char[] password) {
@@ -108,7 +128,9 @@ public class Marketplace {
 
 	/**
 	 * Adds a Product of given ID to the current Account's cart
-	 * @param id - the ID of the Product to add
+	 * 
+	 * @param id
+	 *            - the ID of the Product to add
 	 */
 	public void addToCart(int id) {
 		this.currentAccount.addToCart(id);
@@ -116,19 +138,22 @@ public class Marketplace {
 
 	/**
 	 * Removes a Product of given ID from the current Account's cart
-	 * @param id - the ID of the Product to remove
+	 * 
+	 * @param id
+	 *            - the ID of the Product to remove
 	 */
 	public void removeFromCart(int id) {
 		this.currentAccount.removeFromCart(id);
 	}
-	
+
 	/**
 	 * Gets the total cost of the current Account's cart
+	 * 
 	 * @return the cost value
 	 */
 	public double getCartTotal() {
 		double total = 0.0;
-		
+
 		for (Point p : this.currentAccount.getCart()) {
 			Product product = this.inventory.get(p.x);
 			total += product.getPrice() * p.y;
@@ -146,8 +171,11 @@ public class Marketplace {
 	}
 
 	/**
-	 * Compensates the Seller and Admin while charging the Buyer for the transaction of the passed cart
-	 * @param cart - the cart to fulfill the transaction
+	 * Compensates the Seller and Admin while charging the Buyer for the
+	 * transaction of the passed cart
+	 * 
+	 * @param cart
+	 *            - the cart to fulfill the transaction
 	 */
 	private void conductTransaction(Point[] cart) {
 		for (Point p : cart) {
@@ -160,8 +188,11 @@ public class Marketplace {
 	}
 
 	/**
-	 * Updates quantities in the Marketplace's Inventory corresponding to the cart
-	 * @param cart - the cart whose items to sell
+	 * Updates quantities in the Marketplace's Inventory corresponding to the
+	 * cart
+	 * 
+	 * @param cart
+	 *            - the cart whose items to sell
 	 */
 	private void updateInventory(Point[] cart) {
 		for (Point p : cart) {
@@ -169,20 +200,22 @@ public class Marketplace {
 			product.purchase(p.y);
 		}
 	}
-	
+
 	/**
 	 * Divides the revenue for Admins evenly between each Admin
-	 * @param adminRevenue - the amount of revenue the Admins have received
+	 * 
+	 * @param adminRevenue
+	 *            - the amount of revenue the Admins have received
 	 */
 	private void payAdmins(double adminRevenue) {
 		Account[] admins = this.accounts.getAdmins();
-		
+
 		double revenuePerAdmin = adminRevenue / admins.length;
 		for (Account a : admins) {
 			a.addMoney(revenuePerAdmin);
 		}
 	}
-	
+
 	/**
 	 * @return the current Account
 	 */
@@ -206,7 +239,9 @@ public class Marketplace {
 
 	/**
 	 * Reads the text File at the given filepath to determine global constants
-	 * @param constantsDir - the filepath to read
+	 * 
+	 * @param constantsDir
+	 *            - the filepath to read
 	 */
 	private void loadConstants(String constantsDir) {
 		try {
@@ -221,19 +256,21 @@ public class Marketplace {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Saves the global constants in a text file at the specified filepath
-	 * @param constantsDir - the filepath to save to
+	 * 
+	 * @param constantsDir
+	 *            - the filepath to save to
 	 */
 	private void saveConstants(String constantsDir) {
 		try {
 			File file = new File(constantsDir);
 			PrintWriter out = new PrintWriter(file);
-			
+
 			out.println(ID);
 			out.println(SELLER_REVENUE_PERCENTAGE);
-			
+
 			out.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -241,10 +278,15 @@ public class Marketplace {
 	}
 
 	/**
-	 * Loads all needed resources for the Marketplace to function at the specified filepaths
-	 * @param constantsDir - filepath to save the constants
-	 * @param inventoryDir - filepath to save the Inventory
-	 * @param directoryDir - filepath to save the Directory
+	 * Loads all needed resources for the Marketplace to function at the
+	 * specified filepaths
+	 * 
+	 * @param constantsDir
+	 *            - filepath to save the constants
+	 * @param inventoryDir
+	 *            - filepath to save the Inventory
+	 * @param directoryDir
+	 *            - filepath to save the Directory
 	 */
 	public void loadResources(String constantsDir, String inventoryDir, String directoryDir) {
 		loadConstants(constantsDir);
@@ -254,9 +296,13 @@ public class Marketplace {
 
 	/**
 	 * Saves all resources from the Marketplace at the specified filepaths
-	 * @param constantsDir - filepath to save the constants
-	 * @param inventoryDir - filepath to save the Inventory
-	 * @param directoryDir - filepath to save the Directory
+	 * 
+	 * @param constantsDir
+	 *            - filepath to save the constants
+	 * @param inventoryDir
+	 *            - filepath to save the Inventory
+	 * @param directoryDir
+	 *            - filepath to save the Directory
 	 */
 	public void saveResources(String constantsDir, String inventoryDir, String directoryDir) {
 		saveConstants(constantsDir);
