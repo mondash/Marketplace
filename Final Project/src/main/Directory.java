bb075da5fc9175d76d4e2e5656debfa8b5b7bb90
Nate Pellant <npellant@umail.iu.edu>
2017-04-28 20:40:56 -0400

    Directory modifications
    Javadoc

--- Final Project/src/main/Directory.java
+++ Final Project/src/main/Directory.java
@@ -1,19 +1,41 @@
 package main;
+
 import java.io.File;
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author MattOndash
+ * @author NatePellant
+ *
+ *<p>
+ * Directory stores an ArrayList of Accounts as well as includes useful methods to interpret and mutate this data
+ */
 public class Directory {
 
+	// member variables
 	private ArrayList<Account> accounts;
 	
+	/**
+	 * Directory Constructor: initializes the ArrayList of Accounts
+	 */
 	public Directory() {
 		accounts = new ArrayList<Account>();
 	}
 	
+	/**
+	 * Adds an Account to the ArrayList
+	 * @param account - the Account to add
+	 */
 	public void add(Account account) {
 		this.accounts.add(account);
 	}
 	
+	/**
+	 * Gets the Account with given ID
+	 * @param id - the Account ID
+	 * @return the Account if it exists otherwise returns null
+	 */
 	public Account get(int id) {
 		for (Account a : this.accounts) {
 			if (a.getID() == id) {
@@ -23,6 +45,11 @@ public class Directory {
 		return null;
 	}
 	
+	/**
+	 * Searches for the Account of given name
+	 * @param name - the specified name
+	 * @return whether such Account was found
+	 */
 	public boolean inDir(String name) {
 		if (get(name) != null) {
 			return true;
@@ -31,6 +58,11 @@ public class Directory {
 		}
 	}
 	
+	/**
+	 * Gets the Account with given name
+	 * @param name - the specified name
+	 * @return the Account if it exists, otherwise null
+	 */
 	public Account get(String name) {
 		for (Account a : this.accounts) {
 			if (a.getName().equals(name)) {
@@ -40,6 +72,9 @@ public class Directory {
 		return null;
 	}
 	
+	/**
+	 * @return an Array of all Accounts that are of type Admin
+	 */
 	public Account[] getAdmins() {
 		ArrayList<Account> admins = new ArrayList<Account>();
 		
@@ -54,6 +89,10 @@ public class Directory {
 		return adminArray;
 	}
 	
+	/**
+	 * loads each Account in the given Directory and adds it to the ArrayList
+	 * @param dir - the specified Directory
+	 */
 	public void loadAccounts(String dir) {
 		
 		File folder = new File(dir);
@@ -64,10 +103,13 @@ public class Directory {
 		}
 	}
 	
+	/**
+	 * saves all Accounts by calling each one's write method 
+	 * @param dir - the Directory passed to the Account's write method
+	 */
 	public void saveAccounts(String dir) {
 		for (Account a : this.accounts) {
 			a.writeToFile(dir);
 		}
 	}
-
 }
