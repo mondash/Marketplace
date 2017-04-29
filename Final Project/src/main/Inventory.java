fb9254a17e6d780c68dd0d5bb907f947e859d4bd
Nate Pellant <npellant@umail.iu.edu>
2017-04-28 20:54:55 -0400

    Inventory modifications
    Javadocs

--- Final Project/src/main/Inventory.java
+++ Final Project/src/main/Inventory.java
@@ -1,18 +1,36 @@
 package main;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 
+ * @author MattOndash
+ * @author NatePellant
+ *
+ *<p>
+ *Inventory contains an ArrayList of Products and also includes useful methods to interpret and mutate this data 
+ */
 public class Inventory {
 
+	// member variables
 	private ArrayList<Product> products;
 
+	/**
+	 * Inventory Constructor: initializes a new ArrayList of Products
+	 */
 	public Inventory() {
 		products = new ArrayList<Product>();
 	}
 
+	/**
+	 * Gets the Product with given ID
+	 * @param productID - the specified ID
+	 * @return the selected Product if it exists, otherwise null
+	 */
 	public Product get(int productID) {
 
 		for (Product p : this.products) {
@@ -23,16 +41,10 @@ public class Inventory {
 		return null;
 	}
 
-	/*public Product get(String productName) {
-
-		for (Product p : this.products) {
-			if (p.getName().equals(productName)) {
-				return p;
-			}
-		}
-		return null;
-	}*/
-
+	/**
+	 * Adds the Product to the ArrayList and sorts the ArrayList for aesthetic viewing
+	 * @param product - the Product to add to the ArrayList
+	 */
 	public void add(Product product) {
 		this.products.add(product);
 		// sorts products by name for convenient viewing in GUI
@@ -43,16 +55,10 @@ public class Inventory {
 			}
 		});
 	}
-
-	/*public String[] getProductNames() {
-		String[] names = new String[this.products.size()];
-
-		for (int i = 0; i < this.products.size(); i++) {
-			names[i] = this.products.get(i).getName();
-		}
-		return names;
-	}*/
 	
+	/**
+	 * @return an Array of all Product IDs
+	 */
 	public int[] getProductIDs() {
 		int[] IDs = new int[this.products.size()];
 		
@@ -62,6 +68,11 @@ public class Inventory {
 		return IDs;
 	}
 	
+	/**	 
+	 * Gets all Product IDs which correspond to any of the categories
+	 * @param catNames - the Array of category names
+	 * @return an integer Array of all Product IDs fulfilling the aforementioned condition
+	 */
 	public int[] getProductIDs(String[] catNames) {
 		ArrayList<Integer> IDs = new ArrayList<Integer>();
 		
@@ -80,6 +91,9 @@ public class Inventory {
 		return IDs.stream().mapToInt(i->i).toArray();
 	}
 	
+	/**
+	 * @return an Array of every Product's identifier
+	 */
 	public String[] getProductIdentifiers() {
 		String[] identifiers = new String[this.products.size()];
 		
@@ -90,6 +104,11 @@ public class Inventory {
 		return identifiers;
 	}
 	
+	/**
+	 * 
+	 * @param sellerID - the ID of the Seller
+	 * @return an Array of the identifiers of each Product with given Seller ID
+	 */
 	public String[] getProductIdentifiers(int sellerID) {
 		ArrayList<String> identifiers = new ArrayList<String>();
 
@@ -102,6 +121,11 @@ public class Inventory {
 		return identifiers.toArray(identifierArray);
 	}
 	
+	/**	 
+	 * Gets all Product IDs which correspond to the Seller ID
+	 * @param sellerID - the ID of the Seller
+	 * @return an integer Array of all Product IDs fulfilling the aforementioned condition
+	 */
 	public int[] getProductIDs(int sellerID) {
 		ArrayList<Integer> IDs = new ArrayList<Integer>();
 		
@@ -114,6 +138,9 @@ public class Inventory {
 		return IDs.stream().mapToInt(i->i).toArray();
 	}
 
+	/**
+	 * @return an Array of all categories in the Inventory without duplication 
+	 */
 	public String[] getCategories() {
 		Set<String> categories = new HashSet<String>();
 
@@ -126,6 +153,10 @@ public class Inventory {
 		return names;
 	}
 	
+	/**
+	 * loads each Product in the Inventory by calling its read method
+	 * @param dir - the Directory to look for files
+	 */
 	public void loadProducts(String dir) {
 		
 		File folder = new File(dir);
@@ -136,6 +167,10 @@ public class Inventory {
 		}
 	}
 	
+	/**
+	 * saves each Product in the Inventory by calling its write method
+	 * @param dir - the Directory to save the files
+	 */
 	public void saveProducts(String dir) {
 		for (Product p : this.products) {
 			p.writeToFile(dir);
