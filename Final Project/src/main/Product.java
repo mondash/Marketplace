df8009dc5d84399d4fd6ce6685bf7781d8479180
Nate Pellant <npellant@umail.iu.edu>
2017-04-28 20:29:06 -0400

    Product modifications
    Adding javadocs to Product again

--- Final Project/src/main/Product.java
+++ Final Project/src/main/Product.java
@@ -1,11 +1,19 @@
 package main;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author MattOndash
+ * @author NatePellant
+ *
+ */
 public class Product {
 
+	// member variables
 	private int itemNumber;
 	private int sellerID;
 	private String name;
@@ -14,6 +22,16 @@ public class Product {
 	private double price;
 	private int quantity;
 
+	/**
+	 * Product Constructor: 
+	 * @param itemNumber - integer ID of the Product
+	 * @param sellerID - ID of the Seller of the Product
+	 * @param name - name of the Product
+	 * @param description - short description of the Product
+	 * @param category - the category of the Product
+	 * @param price - the price of the Product
+	 * @param quantity - the quantity of the Product
+	 */
 	public Product(int itemNumber, int sellerID, String name, String description, String category, double price,
 			int quantity) {
 		this.itemNumber = itemNumber;
@@ -25,64 +43,112 @@ public class Product {
 		this.quantity = quantity;
 	}
 	
+	/**
+	 * @return the ID of the Product
+	 */
 	public int getItemID() {
 		return this.itemNumber;
 	}
 	
+	/**
+	 * @return the ID of the Seller
+	 */
 	public int getSellerID() {
 		return this.sellerID;
 	}
 	
+	/**
+	 * @return the name of the Product
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * @param name - the name to set the Product
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * @return a String conjuncting the name of the Product and its item number
+	 */
 	public String getIdentifier() {
 		return this.name + ": " + this.itemNumber;
 	}
 	
+	/**
+	 * @return the description of the Product
+	 */
 	public String getDescription() {
 		return this.description;
 	}
 	
+	/**
+	 * @param desc - the description to set the Product
+	 */
 	public void setDescription(String desc) {
 		this.description = desc;
 	}
 	
+	/**
+	 * @return the category of the Product
+	 */
 	public String getCategory() {
 		return this.category;
 	}
 	
+	/**
+	 * @param category - the category to set the Product 
+	 */
 	public void setCategory(String category) {
 		this.category = category;
 	}
 	
+	/**
+	 * @return the price of the Product
+	 */
 	public double getPrice() {
 		return this.price;
 	}
 	
+	/**
+	 * @param cost - the cost to set the Product
+	 */
 	public void setPrice(double cost) {
 		this.price = cost;
 	}
 	
+	/**
+	 * @return the quantity of the Product
+	 */
 	public int getQuantity() {
 		return this.quantity;
 	}
 
+	/**
+	 * Subtracts the amount from the Product's quantity; includes a safety catch as to disallow negative quantities
+	 * @param amount - the number of Products to purchase
+	 */
 	public void purchase(int amount) {
 		if (amount > this.quantity)
 			amount = quantity;
 		this.quantity -= amount;
 	}
 	
+	/**
+	 * @param amount - the amount to set the Product quantity
+	 */
 	public void setQuantity(int amount) {
 		this.quantity = amount;
 	}
 	
+	/**
+	 * Reads a text file and interprets it as a Product
+	 * @param file - the File to read
+	 * @return a Product given the interpreted data
+	 */
 	public static Product readFromFile(File file) {
 
 		try {
@@ -109,6 +175,10 @@ public class Product {
 		}
 	}
 
+	/**
+	 * Saves Product by writing it to a text File
+	 * @param dir - the directory of the File
+	 */
 	public void writeToFile(String dir) {
 
 		try {
